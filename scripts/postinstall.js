048358a9da1d13d2120c27df04ac9c1fa3506755
nake89 <nake89@gmail.com>
2022-03-26 23:59:39 +0200

    changed project name

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -4,15 +4,9 @@ const os = require('os')
 const shellEnv = process.env.SHELL.split('/')
 const shell = shellEnv[shellEnv.length - 1]
 if (shell === 'zsh') {
-  fs.appendFileSync(
-    `${os.homedir()}/.zshrc`,
-    '\nlau() { launch "$@"; cd $1 }\n'
-  )
+  fs.appendFileSync(`${os.homedir()}/.zshrc`, '\nmene() { men "$@"; cd $1 }\n')
 } else if (shell === 'bash') {
-  fs.appendFileSync(
-    `${os.homedir()}/.bashrc`,
-    '\nlau() { launch "$@"; cd $1 }\n'
-  )
+  fs.appendFileSync(`${os.homedir()}/.bashrc`, '\nmene() { men "$@"; cd $1 }\n')
 } else {
   console.log("Can't find shell")
 }
